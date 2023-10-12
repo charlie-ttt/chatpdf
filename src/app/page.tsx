0dbdbb5bfdae24d6ba2ecaaa42f4f4b0d94b21f0
Charlie <achanont@gmail.com>
2023-10-12 16:48:31 +0700

    customize css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,11 +21,13 @@ export default async function Home() {
     }
   }
   return (
-    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+    <div className="w-screen min-h-screen bg-gradient-to-br from-gray-700 via-gray-900 to-black">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <h1 className="mr-3 mb- text-5xl font-semibold text-slate-300">
+              PDF Summarizer
+            </h1>
             <UserButton afterSignOutUrl="/" />
           </div>
 
@@ -44,9 +46,9 @@ export default async function Home() {
             )}
           </div>
 
-          <p className="max-w-xl mt-1 text-lg text-slate-600">
-            Join millions of students, researchers and professinals to instantly
-            anwer questions and understand research with AI
+          <p className="max-w-xl mt-1 text-lg text-slate-400">
+            Upload a PDF file and you can get answers instantly by asking any
+            questions
           </p>
 
           <div className="w-full mt-4">
